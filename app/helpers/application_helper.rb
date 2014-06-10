423bd4999454ed51bc18807f0b4aedf38b941095
W-Sarah <sarahweiss75@gmail.com>
2014-06-10 21:01:15 +0900

    adding helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def url_fix(link)
+    if link.start_with?("http")
+      return link
+    else return "http://#{link}"
+    end
+  end
+
+  def date_fix(date)
+    date.strftime("%a %d %b %y, at %I:%M %p")
+  end
 end
